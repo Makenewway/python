1fc040b3c73d760b92539a413b273eb57cb762a2
Makenewway <karthikaug02@gmail.com>
2016-11-21 12:08:05 +0530

    exchange of values between variables

---
+++ exchange.py
@@ -0,0 +1,11 @@
+t=0
+a=raw_input('Enter the input of a:')
+b=raw_input('Enter the value of b:')
+c=raw_input('Enter the value of c:')
+t=a
+a=c
+c=b
+b=t
+print "The value of a is %s" %a
+print "The value of b is %s" %b
+print "The value of c is %s" %c
